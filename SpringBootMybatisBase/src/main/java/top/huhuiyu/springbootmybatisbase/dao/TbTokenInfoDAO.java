f08cfe38210c5ff42b65268be78bee50c094a5a0
DarkKnightVip <huhuiyu.vip@gmail.com>
2018-10-24 12:31:47 +0800

    重构mapper和dao名称一致，使用package方式导入mapper

--- SpringBootMybatisBase/src/main/java/top/huhuiyu/springbootmybatisbase/dao/TbTokenInfoDAO.java
+++ SpringBootMybatisBase/src/main/java/top/huhuiyu/springbootmybatisbase/dao/TbTokenInfoDAO.java
@@ -13,11 +13,39 @@ import top.huhuiyu.springbootmybatisbase.entity.TbTokenInfo;
 @Mapper
 public interface TbTokenInfoDAO {
 
+  /**
+   * -查询图片校验码是否存在
+   * 
+   * @param tokenInfo
+   * @return
+   * @throws Exception
+   */
   TbTokenInfo queryImageCodeByToken(TbTokenInfo tokenInfo) throws Exception;
 
+  /**
+   * -添加图片校验码
+   * 
+   * @param tokenInfo
+   * @return
+   * @throws Exception
+   */
   int addImageCode(TbTokenInfo tokenInfo) throws Exception;
 
+  /**
+   * -更新图片校验码
+   * 
+   * @param tokenInfo
+   * @return
+   * @throws Exception
+   */
   int updateImageCode(TbTokenInfo tokenInfo) throws Exception;
 
+  /**
+   * -删除图片校验码
+   * 
+   * @param tokenInfo
+   * @return
+   * @throws Exception
+   */
   int deleteImageCode(TbTokenInfo tokenInfo) throws Exception;
 }
